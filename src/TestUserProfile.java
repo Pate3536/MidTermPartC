e151ebefde501dc635411c8bd7efc1fcd1c237c3
pate3536 <pate3536@sheridancollege.ca>
2022-02-23 10:59:15 -0500

    Added new class

---
+++ src/TestUserProfile.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author princ
+ */
+public class TestUserProfile
+{
+    public static void main(String[]args)
+    {
+    Scanner sc = new Scanner(System.in);
+     
+    System.out.println("enter your name");
+    String name = sc.nextLine();
+     System.out.println("Select eny geners number");
+     int a = sc.nextInt();
+     
+    
+    }
+           
+         
+}
